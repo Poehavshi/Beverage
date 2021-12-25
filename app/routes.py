c36332fb6a95e3c298ef559c54747f3b8245d5f3
Poehavshi <cotniko-arkadi@yandex.ru>
2021-12-25 15:20:37 +0400

    Added index page to the web site with mock content

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'name': 'Ivan', 'surname': 'Ivanov'}
+    applications = [
+        {
+            'author': {'name': 'John', 'surname': 'Johness'},
+            'description': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'name': 'Susan', 'surname': 'Susanness'},
+            'description': 'The Avengers movie was so cool!'
+        }, 
+        {
+            'author': {'name': 'Ипполит', 'surname': 'Ипполитович'},
+            'description': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, applications = applications)
